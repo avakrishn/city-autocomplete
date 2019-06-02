cae38c5f47a43a431d1570821c356101c1440d9c
avakrishn <avakrishn@gmail.com>
2019-06-02 00:21:24 -0700

    restructure js into object oriented design

--- src/js/app.js
+++ src/js/app.js
@@ -1,17 +1,28 @@
 'use-strict';
-const places_endpoint = './src/js/cities.json';
-window.onload = async () => {
 
-    // Async Await
-    const places = await fetch(places_endpoint);
-    const data = await places.json();
-    console.log(data);
+const App = {
+    places_endpoint : './src/js/cities.json',
 
-    // Promises
-    // fetch(places_endpoint)
-    //     .then(res => res.json())
-    //     .then(data => console.log(data));
+    loadPlaces : async () =>{
+        // Async Await
+        const places_response = await fetch(App.places_endpoint);
+        const places = await places_response.json();
+        console.log(places);
 
+        // Promises
+        // const places_promise_data = [];
+        // fetch(places_endpoint)
+        //     .then(res => res.json())
+        //     .then(data => {
+        //         places_promise_data.push(...data);
+        //         console.log(places_promise_data);
+        //     });
+    },
+
+};
+
+
+window.onload = () => {
+    App.loadPlaces();
 }
-
 
