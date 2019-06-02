a8311cf7945eee9b0e3fbf4c8c018458b27fac10
avakrishn <avakrishn@gmail.com>
2019-06-02 12:41:09 -0700

    init bind Events function

--- src/js/app.js
+++ src/js/app.js
@@ -3,11 +3,10 @@
 const App = {
     places_endpoint : './src/js/cities.json',
 
-    loadPlaces : async () =>{
-        // Async Await
+    fetchPlaces : async () =>{
+        // Async Await 
         const places_response = await fetch(App.places_endpoint);
         const places = await places_response.json();
-        console.log(places);
 
         // Promises
         // const places_promise_data = [];
@@ -17,12 +16,32 @@ const App = {
         //         places_promise_data.push(...data);
         //         console.log(places_promise_data);
         //     });
+
+        App.bindEvents(places);
     },
+
+    findMatches: (strToMatch, places) => {
+        
+    },
+
+    bindEvents : (places) =>{
+        console.log(places);
+        const input = document.querySelector('input.search');
+        input.addEventListener('change', (event) => {
+            console.log(event.target.value);
+        });
+
+        input.addEventListener('keydown', (event) => {
+            console.log(event.target.value);
+        });
+
+    }, 
+
 
 };
 
 
 window.onload = () => {
-    App.loadPlaces();
+    App.fetchPlaces();
 }
 
